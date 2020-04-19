98981767e1bc1bb751da3958f17eec3eab285e68
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-18 21:37:45 -0500

    Build on Game: added stepNumber to state, defined jumpTo(), updated handleClick to update stepNumber

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -99,6 +99,7 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext:true,
     };
   }
@@ -116,6 +117,12 @@ class Game extends React.Component {
       }]),
       xIsNext: !this.state.xIsNext,
     });
+  }
+  jumpTo(step) {
+    this.setState({
+    stepNumber: step,
+    xIsNext: (step % 2) === 0, //this sets xIsNext to True if stepNumber is even --> ???redundant???
+    })
   }
   render() {
     const history = this.state.history;
